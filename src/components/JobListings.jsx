7f430d7e08670e259955558389e823e6dc9c0859
akyarv0 <akyarv0@gmail.com>
2024-05-29 17:39:50 +0300

    links

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,36 +1,32 @@
 import jobs from "../jobs.json";
+import JobListing from "./JobListing";
 
-const JobListing = () => {
-  console.log(jobs);
-  
+const JobListings = ({isHome=false}) => {
+
+  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+
+
+
+
   return (
     <div>
-      {" "}
-      <section className='bg-blue-50 px-4 py-10'>
-      <div className='container-xl lg:container m-auto'>
-        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
-        Browse Jobs
-        </h2>
+      <section className="bg-blue-50 px-4 py-10">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+            {isHome ? "Recent Jobs" : "Popular Jobs"}
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobListings.map((job) => (
 
-       
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {jobs.map((job) => (
-              <div
-                key={job.id}
-                className='bg-white p-6 rounded-lg shadow-md'
-              >
-                <h3 className='text-xl font-bold mb-2'>{job.title}</h3>
-                <p className='text-gray-700'>{job.description}</p>
+              <JobListing key={job.id} job={job} />
 
-              </div>
+            
             ))}
-      
           </div>
-
-      </div>
-    </section>
+        </div>
+      </section>
     </div>
   );
 };
 
-export default JobListing;
+export default JobListings;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero";
+import HomeCard from "../components/HomeCard";
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Hero />
+      <HomeCard />
+      <JobListings isHome={true} />
+      <ViewAllJobs />
+    </div>
+  );
+};
+
+export default HomePage;
