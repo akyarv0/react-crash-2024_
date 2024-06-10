1f24972adba7c43d5e1405305fc73a712b817552
akyarv0 <akyarv0@gmail.com>
2024-06-10 04:03:50 +0300

    added

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -241,7 +241,7 @@ const AddJobPage = ({addJobSubmit}) => { // addJobSubmit fonksiyonu ekledim
                   className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                   type="submit"
                 >
-                  Add Job
+                  Add New Job
                 </button>
               </div>
             </form>
